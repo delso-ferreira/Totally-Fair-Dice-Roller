3cb851f4af6e9f8abb92fb81e13c4968692f4401
delso-ferreira <delsofelipe@gmail.com>
2023-11-21 13:30:34 -0300

    Refactoring Options

--- src/index.js
+++ src/index.js
@@ -10,52 +10,55 @@ const client = new Client({
     ]
 });
 
+function validateAndGetOptions(interaction) {
+    const quantity = interaction.options.getNumber('quantity');
+    const sides = interaction.options.getNumber('sides');
+ 
+    if (!quantity || !sides) {
+        interaction.reply('Por favor, forneça valores válidos para a quantidade e os lados.');
+        return null;
+    }
+ 
+    if (quantity.toString().length > 2 || sides.toString().length > 2) {
+        interaction.reply('Digite no máximo dois dígitos');
+        return null;
+    }
+ 
+    return { quantity, sides };
+ }
+ 
+
 client.on('ready', () => {
     console.log(`${client.user.tag} está rodando`);
 });
 
 client.on('interactionCreate', async (interaction) => {
     if (!interaction.isCommand()) return;
-
-    const { commandName, options } = interaction;
-
+  
+    const { commandName } = interaction;
+  
     if (commandName === 'roll') {
-        const quantity = options.getNumber('quantity');
-        const sides = options.getNumber('sides');
-
-        if (!quantity || !sides) {
-            return interaction.reply('Por favor, forneça valores válidos para a quantidade e os lados.');
-        }
-
-        if (quantity.toString().length > 2 || sides.toString().length > 2) {
-            return interaction.reply('Digite no máximo dois dígitos');
-        }   
-        
-        const results = rollDice(quantity, sides);
+        const options = validateAndGetOptions(interaction);
+        if (!options) return;
+  
+        const results = rollDice(options.quantity, options.sides);
         const resultsString = results.join(', ');
-
+  
         interaction.reply(`Resultados dos dados: ${resultsString}`);
     }
     
     if (commandName === 'fair_roll') {
-        const quantity = options.getNumber('quantity');
-        const sides = options.getNumber('sides');
-
-        if (!quantity || !sides) {
-            return interaction.reply('Por favor, forneça valores válidos para a quantidade e os lados.');
-        }
-
-        if (quantity.toString().length > 2 || sides.toString().length > 2) {
-            return interaction.reply('Digite no máximo dois dígitos');
-        }   
-
-        const results = rollFairDice(quantity, sides);
+        const options = validateAndGetOptions(interaction);
+        if (!options) return;
+  
+        const results = rollFairDice(options.quantity, options.sides);
         const resultsString = results.join(', ');
-
+  
         interaction.reply(`Resultado que deveria ter sido: ${resultsString}`);
-
     }
-});
+  });
+  
+ 
 
 function rollDice(quantity, sides) {
     const results = [];
